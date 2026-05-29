4286a92e55459428884ae3b09037f75546759cf8
GeonRyoung <rjsfud7467@naver.com>
2026-05-29 21:53:54 +0900

    FIFO

---
+++ OS_Project/PolicyBase.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <vector>
+
+enum class PageStatus {
+    Hit,
+    Fault,
+    Migration
+};
+
+struct StepRecord {
+    char reqChar;                 
+    PageStatus status;            
+    int targetRow;                
+    std::vector<char> memorySnap; 
+};
+
+class PolicyBase {
+protected:
+    int frameSize;
+    int hitCount = 0;
+    int faultCount = 0;
+    int migrationCount = 0;
+
+    std::vector<StepRecord> history; 
+    std::vector<float> timeData;
+    std::vector<float> faultRateData;
+
+public:
+    PolicyBase(int size) : frameSize(size) {}
+    virtual ~PolicyBase() = default;
+
+    virtual void Operate(char data) = 0; 
+
+    int GetHit() const { return hitCount; }
+    int GetFault() const { return faultCount; }
+    int GetMigration() const { return migrationCount; }
+    int GetFrameSize() const { return frameSize; } // ◀ 안전한 렌더링을 위해 추가!
+    const std::vector<StepRecord>& GetHistory() const { return history; }
+    const std::vector<float>& GetTimeData() const { return timeData; }
+    const std::vector<float>& GetFaultRateData() const { return faultRateData; }
+};
